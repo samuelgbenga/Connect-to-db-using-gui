b091f42185f9bf4775ae04fdaebb1034199f47c5
samuelgbenga <samuelgbenga972@gmail.com>
2024-05-28 22:08:51 +0100

    my first fail to load to db

---
+++ src/main/java/com/secondspringproject/connect_to_db_using_gui/ConnectToDbUsingGuiApplication.java
@@ -0,0 +1,15 @@
+package com.secondspringproject.connect_to_db_using_gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectToDbUsingGuiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConnectToDbUsingGuiApplication.class, args
+		);
+	}
+
+}
---
+++ src/test/java/com/secondspringproject/connect_to_db_using_gui/ConnectToDbUsingGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondspringproject.connect_to_db_using_gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectToDbUsingGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
